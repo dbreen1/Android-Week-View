ccef344fff92f7738828cf05a7fed0d1578cbbf4
Darren Breen <darren.m.breen@outlook.com>
2022-03-19 16:07:39 +0000

    fix

--- core/src/main/java/com/alamkanak/weekview/HeaderRenderer.kt
+++ core/src/main/java/com/alamkanak/weekview/HeaderRenderer.kt
@@ -80,13 +80,13 @@ private class HeaderUpdater(
     private val animator = ValueAnimator()
 
     override fun update() {
-        val missingDates = viewState.dateRange.filterNot { labelLayouts.hasKey(1) }
+        val missingDates = viewState.dateRange.filterNot { labelLayouts.hasKey(it.toEpochDays()) }
         for (date in missingDates) {
             val key = date.toEpochDays()
             labelLayouts.put(key, calculateStaticLayoutForDate(date))
         }
 
-        val dateLabels = viewState.dateRange.map { labelLayouts[1] }
+        val dateLabels = viewState.dateRange.map { labelLayouts[it.toEpochDays()] }
         updateHeaderHeight(dateLabels)
     }
 
