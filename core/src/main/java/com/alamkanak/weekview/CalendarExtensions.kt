3b724ae18c42814b906965450657595ccc6d02ed
Darren Breen <darren.m.breen@outlook.com>
2022-03-19 16:14:07 +0000

    revert test

--- core/src/main/java/com/alamkanak/weekview/CalendarExtensions.kt
+++ core/src/main/java/com/alamkanak/weekview/CalendarExtensions.kt
@@ -137,7 +137,7 @@ internal val Calendar.isBeforeToday: Boolean
 internal val Calendar.isToday: Boolean
     get() = isSameDate(today())
 
-internal fun Calendar.toEpochDays(): Int = ( this.get(Calendar.YEAR) + this.get(Calendar.MONTH) + this.get(Calendar.DAY_OF_MONTH)  ) 
+internal fun Calendar.toEpochDays(): Int = (atStartOfDay.timeInMillis / DAY_IN_MILLIS).toInt()
 
 internal fun Calendar.toIntKey(): Int = ( this.get(Calendar.YEAR) + this.get(Calendar.MONTH) + this.get(Calendar.DAY_OF_MONTH)  ) 
 
